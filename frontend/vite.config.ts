a86bbde6c95eeec2f94f22d54537da1bf51d32dc
n3waz <lhk@cesar.school>
2026-01-13 09:25:40 -0300

    build: added frontend build dependencies

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    },
   },
 })
